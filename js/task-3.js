b2ba27f65c97f863a1215658c5aaaee80e42fbbf
AnnaP-G <klientkv1511@gmail.com>
2023-12-26 18:05:44 +0200

    Homework06

--- js/task-3.js
+++ js/task-3.js
@@ -1,23 +1,25 @@
 class StringBuilder {
+    #value;
+
     constructor(initialValue) {
-        this.value = initialValue;
-}
+        this.#value = initialValue;
+    }
 
-getValue() {
-    return this.value;
-}
+    getValue() {
+        return this.#value;
+    }
 
-padEnd(str) {
-    this.value += str;
-}
+    padEnd(str) {
+        this.#value += str;
+    }
 
-padStart(str) {
-    this.value = str + this.value;
-}
+    padStart(str) {
+        this.#value = `${str}${this.#value}`;
+    }
 
-padBoth(str) {
-    this.value = str + this.value + str;
-}
+    padBoth(str) {
+        this.#value = `${str}${this.#value}${str}`
+    }
 }
 
 const builder = new StringBuilder(".");
